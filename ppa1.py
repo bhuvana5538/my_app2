5202826efd4d4981055887bbed63603dfd422297
bhuvana5538 <155609641+bhuvana5538@users.noreply.github.com>
2025-06-06 13:55:05 +0530

    Update ppa1.py

--- ppa1.py
+++ ppa1.py
@@ -13,7 +13,8 @@ column_map = {
     "LeetCode": "Paste your Leetcode profile link"
 }
 
-FASTAPI_URL = "http://localhost:8000"  # Change to your deployed backend URL
+FASTAPI_URL = "https://fastapi-backend.onrender.com"
+  # Change to your deployed backend URL
 
 def extract_github_username(value):
     from urllib.parse import urlparse
